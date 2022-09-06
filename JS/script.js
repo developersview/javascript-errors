ffe255c04751a227e361a406b97929d3e5260112
developersview <pcslg1998@gmail.com>
2022-09-06 14:48:07 +0530

    range error

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,20 @@
 /* const arr = [5, 6, 7, 8];
 arr.length = 90 ** 99;
 console.log(arr); */
-
+const check = n => {
+    if (n < 500 && n > 100) {
+        console.log(`${n} is valid`);
+    } else {
+        throw new RangeError(`Argument must be in 100 to 500 range`);
+    }
+};
+try {
+    check(800);
+} catch (error) {
+    if (error instanceof RangeError) {
+        console.log(error);
+    }
+}
 //reference error
 //syntax error
 //typeerror
